3006ddb794d4d4902f0fef601dd3f4e7b29f8031
KohnoTaiyo <59274850+KohnoTaiyo@users.noreply.github.com>
2023-02-27 16:50:56 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.otf$/i,
+      type: "asset/resource",
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
